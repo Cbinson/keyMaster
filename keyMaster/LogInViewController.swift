99910f28871a793c6b36afb281439fa30a83f910
robinson <robinson@robinsondeMacBook-Pro.local>
2017-03-19 22:19:24 +0800

    first commit

---
+++ keyMaster/LogInViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LogInViewController.swift
+//  keyMaster
+//
+//  Created by robinson on 2017/3/19.
+//  Copyright © 2017年 robinson. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
